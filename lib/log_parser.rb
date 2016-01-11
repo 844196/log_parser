08f316938410f2c8aa5beb1779d19569b9d11b99
Masaya Takeda <844196@gmail.com>
2016-01-12 02:34:25 +0900

    add module tree

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -1,2 +1,36 @@
+module LogParser
+  class Configuration
+    # 設定値格納クラス
+  end
+
+  class << self
+    # 設定値参照・変更用メソッドがある
+  end
+
+  module Record
+    # レコードパーサーがある
+  end
+
+  module Record::Content
+    # レコードオブジェクト
+
+    class Base
+      # レコード要素の抽象基底クラス
+    end
+
+    class User < Base
+      # ユーザIDを格納
+    end
+
+    class PC < Base
+      # クライアントIDを格納
+    end
+
+    class UseDateTime < Base
+      # 使用日時を格納
+    end
+  end
+end
+
 # load
 require 'log_parser/version'
