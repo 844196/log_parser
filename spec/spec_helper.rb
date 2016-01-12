d1952d83481527c781042c8cc8337ad50ee43e6f
Masaya Takeda <844196@gmail.com>
2016-01-13 00:58:54 +0900

    add Coveralls of CI as a service

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,10 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'coveralls'
 require 'log_parser'
 
 TEST_CONFIG_YAML_PATH = File.expand_path('../test_config.yml', __FILE__)
+
+Coveralls.wear!
 
 RSpec.configure do |config|
   config.before(:suite) { puts "\e[H\e[2J" }
